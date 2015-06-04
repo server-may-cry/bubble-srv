26e26e61e2bd011d0a83a71b13c146d7ed7c9a79
Opletaev Sergey <opletaev1220@yandex.ru>
2015-06-04 21:17:28 +0300

    debug

--- app/routes/test.php
+++ app/routes/test.php
@@ -1,7 +1,7 @@
 <?php
 
-$app->post('/test', function() use ($app) {
+$app->get('/test', function() use ($app) {
 	$VK_SECRET = getenv('VK_SECRET');
-
-	render( ['vk'=>substr($VK_SECRET, 0, 4)] );
+	$BULLET_ENV = getenv('BULLET_ENV');
+	render( ['vk'=>substr($VK_SECRET, 0, 4), 'BULLET_ENV'=>$BULLET_ENV] );
 });
