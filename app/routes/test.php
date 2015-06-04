0f6e003df5b9febc0ba80150528685f1d1f6ab87
Opletaev Sergey <opletaev1220@yandex.ru>
2015-06-04 21:18:45 +0300

    debug

--- app/routes/test.php
+++ app/routes/test.php
@@ -3,5 +3,13 @@
 $app->get('/test', function() use ($app) {
 	$VK_SECRET = getenv('VK_SECRET');
 	$BULLET_ENV = getenv('BULLET_ENV');
-	render( ['vk'=>substr($VK_SECRET, 0, 4), 'BULLET_ENV'=>$BULLET_ENV] );
+	error_log(
+			json_encode(
+					[
+						'vk'=>$VK_SECRET,
+						'BULLET_ENV'=>$BULLET_ENV,
+					]
+				)
+		);
+	render( 'see error log' );
 });
