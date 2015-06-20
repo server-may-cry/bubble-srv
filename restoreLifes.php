7ab80e41e3c68b213ae6423fd1894d2f286e3416
Opletaev Sergey <opletaev1220@yandex.ru>
2015-06-20 21:54:52 +0300

    debug

--- restoreLifes.php
+++ restoreLifes.php
@@ -1,6 +1,7 @@
 <?php
 $dbopts = parse_url(getenv('DATABASE_URL'));
 var_dump($dbopts);
+error_log('start lf');
 $dbh = new PDO(
 	'pgsql:dbname='.ltrim($dbopts["path"],'/') .
 	';host='.$dbopts["host"] .
@@ -10,9 +11,11 @@ $dbh = new PDO(
 );
 //$dbh = new PDO('mysql:host=localhost;dbname=bubble', 'bubble');
 try{
+	error_log('db exec');
 	$count = $dbh->exec('update user set remaining_tries = 5 where remaining_tries < 5');
 	var_dump($count);
 } catch (Exception $e) {
+	error_log('db exec exception');
 	var_dump('exception :(');
 	var_dump( $e->getMessage() );
 }
