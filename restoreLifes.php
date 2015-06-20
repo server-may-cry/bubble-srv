d31ea16a49bbce8a44ad9a48d1f5d835fa5ae4cf
Opletaev Sergey <opletaev1220@yandex.ru>
2015-06-20 21:51:30 +0300

    debug cron

--- restoreLifes.php
+++ restoreLifes.php
@@ -1,5 +1,6 @@
 <?php
 $dbopts = parse_url(getenv('DATABASE_URL'));
+var_dump($dbopts);
 $dbh = new PDO(
 	'pgsql:dbname='.ltrim($dbopts["path"],'/') .
 	';host='.$dbopts["host"] .
@@ -8,5 +9,10 @@ $dbh = new PDO(
 	$dbopts["pass"]
 );
 //$dbh = new PDO('mysql:host=localhost;dbname=bubble', 'bubble');
-$count = $dbh->exec('update user set remaining_tries = 5 where remaining_tries < 5');
-var_dump($count);
+try{
+	$count = $dbh->exec('update user set remaining_tries = 5 where remaining_tries < 5');
+	var_dump($count);
+} catch (Exception $e) {
+	var_dump('exception :(');
+	var_dump( $e->getMessage() );
+}
