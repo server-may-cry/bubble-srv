218867d598bc16a822a872f5801862abc099f5c3
Sergey Air <sopletaev@gmail.com>
2015-09-12 11:52:07 +0300

    not work

--- src/global.php
+++ src/global.php
@@ -3,7 +3,11 @@ define('ROOT', dirname(__DIR__) . '/');
 define('ROUTE_ROOT', ROOT . 'routes/');
 
 function findUser($uid) {
-    
+    $user = R::findOne('users', 'id = ?', [(int)$uid]);
+
+    if($user === NULL)
+        throw new Exception("UserID: ".$uid.' not found');
+    return $user;
 }
 
 // Throw Exceptions for everything so we can see the errors
