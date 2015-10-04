4cee4449fd3a55c9a56fda92ac14cc73e9f13761
Sergey Opletaev <sopletaev@gmail.com>
2015-10-04 09:51:10 +0300

    init route

---
+++ routes/upload.php
@@ -0,0 +1,24 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+use Alchemy\Zippy\Zippy;
+
+$app->post('/upload', function() use ($app) {
+    $start = microtime(true);
+    $src = fopen(CDN_ROOT.'bubble.zip', 'r');
+    $trg = fopen(ROOT.'bubble.zip', 'w');
+    while($content = fread($src, 10240)) {
+        fwrite($trg, $content);
+    }
+    $zipyy = new Zippy();
+    $archive = $zippy->open('bubble.tar');
+    $archive->extract(ROOT.'web/bubble');
+    $contains = [];
+    foreach ($archive as $member) {
+        $contains[] = (string) $member;
+    }
+    unlink(ROOT.'bubble.zip');
+    $time = microtime(true) - $start;
+    $contains[] = sprintf('Скрипт выполнялся %.4F сек.', $time);
+    return $app->json($contains);
+});
