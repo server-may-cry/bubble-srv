078aaa3c78aeb50e5c4b50f079c6fc4321b0ac29
Sergey <sopletaev@gmail.com>
2015-10-04 20:50:23 +0300

    no output

--- routes/upload.php
+++ routes/upload.php
@@ -7,19 +7,16 @@ $app->post('/upload', function() use ($app) {
     $start = microtime(true);
     $src = fopen(CDN_ROOT.'bubble.zip?'.time(), 'r');
     $trg = fopen(ROOT.'bubble.zip', 'w');
-    while($content = fread($src, 1024000)) {
-        fwrite($trg, $content);
-    }
-    //stream_copy_to_stream($src, $trg);
+    stream_copy_to_stream($src, $trg);
     fclose($src);
     fclose($trg);
     $zippy = Zippy::load();
     $archive = $zippy->open(ROOT.'bubble.zip');
     $archive->extract(ROOT.'web/bubble');
     $contains = [];
-    foreach ($archive as $member) {
-        $contains[] = (string) $member;
-    }
+    //foreach ($archive as $member) {
+    //    $contains[] = (string) $member;
+    //}
     unlink(ROOT.'bubble.zip');
     $time = microtime(true) - $start;
     $contains[] = sprintf('Скрипт выполнялся %.4F сек.', $time);
