03406cdbb30de2e65e34e45ea7cfb275e4ea6da5
Sergey <sopletaev@gmail.com>
2015-10-18 15:32:14 +0300

    code clean

--- src/db.php
+++ src/db.php
@@ -25,15 +25,11 @@ if ($redis_exist) {
     ]);
 }
 
-function restoreLifes($redis_exist, $redis) {
-	try{
-	    $count = R::exec('update users set remaining_tries = 5 where remaining_tries < 5');
-	    //error_log('users restored lifes: '.var_export($count, true) );
-	} catch (Exception $e) {
-	    error_log('db exec exception '.$e->getMessage());
-	}
-
-	if ($redis_exist) {
-		// $redis->eval("return redis.call('del', unpack(redis.call('keys', 'remainingTries:*')))", 0, null);
-	}
+function restoreLifes() {
+    try{
+        $count = R::exec('update users set remaining_tries = 5 where remaining_tries < 5');
+        //error_log('users restored lifes: '.var_export($count, true) );
+    } catch (Exception $e) {
+        error_log('db exec exception '.$e->getMessage());
+    }
 }
