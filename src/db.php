2ec22cf17ef6fe322f70e154fd0381fe4dddc162
Sergey <sopletaev@gmail.com>
2015-10-20 23:47:26 +0300

    return freeze

--- src/db.php
+++ src/db.php
@@ -8,7 +8,7 @@ $dburl = getenv('DATABASE_URL');
 if(strlen($dburl)>0) {
     $dbopts = parse_url($dburl);
     R::setup('pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"].';port='.$dbopts["port"], $dbopts["user"], $dbopts["pass"]);
-    //R::freeze( true );
+    R::freeze( true );
 } else {
     R::setup(); // SQLite in memory
 }
