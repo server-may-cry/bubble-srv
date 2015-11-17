9660713f16c3b6fa1a96b537b41ab9b73db03761
Sergey Opletaev <sopletaev@gmail.com>
2015-11-17 16:38:09 +0300

    add template. fix error

--- command/change_progress_store.php
+++ command/change_progress_store.php
@@ -3,12 +3,22 @@
 
 require __DIR__.'/../src/app.php';
 
+$islandsLevelCount = [
+    array_fill(0,IslandLevels::$count1,-1),
+    array_fill(0,IslandLevels::$count2,-1),
+    array_fill(0,IslandLevels::$count3,-1),
+    array_fill(0,IslandLevels::$count4,-1),
+    array_fill(0,IslandLevels::$count5,-1),
+    array_fill(0,IslandLevels::$count6,-1),
+    array_fill(0,IslandLevels::$count7,-1),
+];
+
 $users = R::findCollection('users');
 while($user = $users->next()) {
     $stars = R::findCollection('star', 'user_id = ? and level_mode = 0', [$user->id]);
-    $progress = [];
+    $progress = $islandsLevelCount;
     while($star = $stars->next()) {
-        $progress[ $star->currentStage ][ $user->completeSubStage ] = $star->completeSubStageRecordStat;
+        $progress[ $star->currentStage ][ $star->completeSubStage ] = $star->completeSubStageRecordStat;
     }
     $user->progressStandart = json_encode($progress);
     R::store($user);
