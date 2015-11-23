a5b2f2d88338a6e50d186112fbae2d8f5a8604cd
Sergey <sopletaev@gmail.com>
2015-11-23 04:34:56 +0300

    prepare aws migration

--- src/db.php
+++ src/db.php
@@ -4,7 +4,7 @@ require_once ROOT.'rb.php'; // RedBeanPHP 4
 require_once ROOT.'vendor/autoload.php'; // for predis client when run from cron
 
 // http://redbeanphp.com/
-$dburl = getenv('DATABASE_URL');
+$dburl = getenv('DB_URL');
 if(strlen($dburl)>0) {
     $dbopts = parse_url($dburl);
     R::setup('pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"].';port='.$dbopts["port"], $dbopts["user"], $dbopts["pass"]);
