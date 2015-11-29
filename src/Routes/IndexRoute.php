14f61c849444e96f8eb1504fb3a9d24dbcc7fde2
Sergey <sopletaev@gmail.com>
2015-11-29 16:39:40 +0300

    prepare migration on aws

--- src/Routes/IndexRoute.php
+++ src/Routes/IndexRoute.php
@@ -27,5 +27,6 @@ abstract class IndexRoute {
 
     public static function loader(Application $app) {
         return new Response('loaderio-b1605c8654686a992bd3968349d85b8e');
+        // loaderio-a1605b7f59f37748149caae19249ff85
     }
 }
