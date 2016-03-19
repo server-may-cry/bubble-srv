0f1465a66d21316c674dfc98576e5c0f3665494b
Sergey Opletaev <sopletaev@gmail.com>
2016-03-19 20:32:25 +0300

    dicrease life time restoration

--- src/config/UserParams.php
+++ src/config/UserParams.php
@@ -7,6 +7,6 @@ abstract class UserParams
     const DEFAULT_REMAINING_TRIES = 5;
     const DEFAULT_CREDITS = 1000;
     const DEFAULT_CREDITS_OK = 3000;
-    const INTERVAL_TRIES_RESTORATION = 3600; // 1 hour
+    const INTERVAL_TRIES_RESTORATION = 1800; // 30 minutes
     const FRIENDS_BONUS_CREDITS_MULTIPLIER = 40;
 }
